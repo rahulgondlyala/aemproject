862dc58f4ef61887c56009a4ea15f4dce9f88d87
Rahul Gondlyala <rahul.gondlyala@tadigital.com>
2022-05-10 20:05:32 +0530

    Updated impl class to include DefaultInjectionStrategy property

--- core/src/main/java/com/ta/core/models/StudentImpl.java
+++ core/src/main/java/com/ta/core/models/StudentImpl.java
@@ -3,12 +3,14 @@ package com.ta.core.models;
 import org.apache.lucene.queries.function.valuesource.MultiFunction;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 
 import javax.inject.Inject;
 
 @Model(adaptables = Resource.class,
-        adapters = Student.class)
+        adapters = Student.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class StudentImpl implements Student{
 
     @Inject       // can also use @ValueMapValue instead of inject
